d2ab22a476e13957c628d6d80ab381562289a0d6
Rahul Ranjan <rahul.rrixe@gmail.com>
2019-10-19 12:07:20 +0800

    Initial Commit

---
+++ PhotopadTests/PhotopadTests.swift
@@ -0,0 +1,34 @@
+//
+//  PhotopadTests.swift
+//  PhotopadTests
+//
+//  Created by Rahul Ranjan on 19/10/19.
+//  Copyright © 2019 Rudrakos. All rights reserved.
+//
+
+import XCTest
+@testable import Photopad
+
+class PhotopadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
